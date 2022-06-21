314825f3dbd2255f5edc80ce66a85c5c1e3c006b
Aindrila Das <aindriladas24@gmail.com>
2022-06-21 23:10:09 +0530

    Update Valid Anagram.py

--- Arrays and Hashing/Valid Anagram.py
+++ Arrays and Hashing/Valid Anagram.py
@@ -16,6 +16,25 @@ Output: false
   
   
 Solution:
-  
+ 
+ 
+class Solution:
+  def isAnagram(self, s: str, t: str):
+   
+   if len(s) != len(t):
+     return False
+    
+   countS, countT = {}, {}
+    
+   for i in range(len(s)):
+     countS[s[i]] = 1 + countS.get(s[i])
+     countT[t[i]] = 1 + countT.get(t[i])
+     
+   for c in countS:
+     if countS[c] != countT.get(c, 0):
+       return False
+      
+   return True
+     
   
   
