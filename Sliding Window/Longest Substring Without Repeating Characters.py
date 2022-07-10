a4c1c2ef699d52cc565de8cd45a6e5424008bf02
Aindrila Das <aindriladas24@gmail.com>
2022-07-10 21:15:12 +0530

    Update Longest Substring Without Repeating Characters.py

--- Sliding Window/Longest Substring Without Repeating Characters.py
+++ Sliding Window/Longest Substring Without Repeating Characters.py
@@ -12,3 +12,23 @@ Example 2:
 Input: s = "bbbbb"
 Output: 1
 Explanation: The answer is "b", with the length of 1.
+
+ 
+ 
+Solution:
+ 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+            
+        return res
+        
