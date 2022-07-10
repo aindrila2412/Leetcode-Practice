80ac503015e683c60c0a47398095f20166c44314
Aindrila Das <aindriladas24@gmail.com>
2022-07-10 21:04:26 +0530

    Update Container With Most Water.py

--- Two Pointers/Container With Most Water.py
+++ Two Pointers/Container With Most Water.py
@@ -18,3 +18,24 @@ Example 2:
 
 Input: height = [1,1]
 Output: 1
+
+ 
+ 
+ 
+Solution:
+ 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        l, r = 0, len(height) - 1
+        
+        while l < r:
+            area = (r - 1) * min(height[l], height[r])
+            res = max(res, area)
+            
+            if height[l] <height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+        
