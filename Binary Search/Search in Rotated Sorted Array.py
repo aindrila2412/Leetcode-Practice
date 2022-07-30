f68ca43095326cf248aced2b3843662ce7b95ac0
Aindrila Das <aindriladas24@gmail.com>
2022-07-30 21:36:23 +0530

    Update Search in Rotated Sorted Array.py

--- Binary Search/Search in Rotated Sorted Array.py
+++ Binary Search/Search in Rotated Sorted Array.py
@@ -16,3 +16,38 @@ Example 2:
 
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
+
+ 
+ 
+ 
+Solution:
+ 
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+            
+            #left sorted portion
+            
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                    
+                else:
+                    r = mid - 1
+            
+            #right sorted portion
+            
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+                    
+        return -1
+        
